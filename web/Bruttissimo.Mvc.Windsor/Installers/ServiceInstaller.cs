b6c80578455d01c8a4d04979af5e78cc31b11cd4
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2012-08-08 19:07:43 -0300

    identation

--- web/Bruttissimo.Mvc.Windsor/Installers/ServiceInstaller.cs
+++ web/Bruttissimo.Mvc.Windsor/Installers/ServiceInstaller.cs
@@ -6,27 +6,27 @@ using Castle.Windsor;
 
 namespace Bruttissimo.Mvc
 {
-	/// <summary>
-	/// Registers all services and their support dependency components from Domain Logic.
-	/// </summary>
-	public class ServiceInstaller : IWindsorInstaller
-	{
-		public void Install(IWindsorContainer container, IConfigurationStore store)
-		{
-			container.Register(
-				AllTypes
-					.FromAssemblyContaining<EmailService>()
-					.Where(t => t.Name.EndsWith("Service"))
-					.WithService.Select(IoC.SelectByInterfaceConvention)
-					.LifestylePerWebRequest()
-			);
+    /// <summary>
+    /// Registers all services and their support dependency components from Domain Logic.
+    /// </summary>
+    public class ServiceInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(
+                AllTypes
+                    .FromAssemblyContaining<EmailService>()
+                    .Where(t => t.Name.EndsWith("Service"))
+                    .WithService.Select(IoC.SelectByInterfaceConvention)
+                    .LifestylePerWebRequest()
+            );
 
-			container.Register(
-				AllTypes
-					.FromAssemblyContaining<AuthenticationPortal>()
-					.Where(t => t.Name.EndsWith("AuthenticationPortal"))
-					.LifestylePerWebRequest()
-			);
-		}
-	}
+            container.Register(
+                AllTypes
+                    .FromAssemblyContaining<AuthenticationPortal>()
+                    .Where(t => t.Name.EndsWith("AuthenticationPortal"))
+                    .LifestylePerWebRequest()
+            );
+        }
+    }
 }
