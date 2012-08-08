9d916f70726274e87ace1830d9f8366ee86b6ac6
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2012-08-08 11:51:40 -0300

    added log entry when a job is autorun

--- web/Bruttissimo.Common.Mvc/IoC/Quartz/JobAutoRunner.cs
+++ web/Bruttissimo.Common.Mvc/IoC/Quartz/JobAutoRunner.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using Quartz;
 using Quartz.Impl;
+using log4net;
 
 namespace Bruttissimo.Common.Mvc
 {
 	public class JobAutoRunner : IJobAutoRunner
 	{
+		private readonly ILog log = LogManager.GetLogger(typeof(JobAutoRunner));
 		private readonly IList<Type> jobTypes;
 
 		public JobAutoRunner(IList<Type> jobTypes)
@@ -22,6 +24,8 @@ namespace Bruttissimo.Common.Mvc
 		{
 			foreach (Type jobType in jobTypes)
 			{
+				log.Debug(Resources.Debug.SchedulingAutoRunJob.FormatWith(jobType.Name));
+
 				JobDetailImpl detail = new JobDetailImpl(jobType.Name, null, jobType);
 				ITrigger trigger = TriggerBuilder.Create().ForJob(detail).StartNow().Build();
 
