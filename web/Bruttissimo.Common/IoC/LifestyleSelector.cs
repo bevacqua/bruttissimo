7584802812c77ac3ec36b7577d132d025387bf45
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2012-08-08 21:11:40 -0300

    updated castle package

---
+++ web/Bruttissimo.Common/IoC/LifestyleSelector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Castle.MicroKernel;
+
+namespace Bruttissimo.Common
+{
+    public class LifestyleSelector : IHandlerSelector
+    {
+        public bool HasOpinionAbout(string key, Type service)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IHandler SelectHandler(string key, Type service, IHandler[] handlers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
