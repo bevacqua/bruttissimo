17a8dbe74cc304d7ff83aaf3513375f17893879c
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2012-08-12 01:45:53 -0300

    updated CanDisplayDebuggingDetails, produced issue #45

--- web/Bruttissimo.Common.Mvc/Extensions/HttpRequestBase.cs
+++ web/Bruttissimo.Common.Mvc/Extensions/HttpRequestBase.cs
@@ -1,16 +1,36 @@
+using System;
+using System.Security.Principal;
 using System.Web;
 
 namespace Bruttissimo.Common.Mvc
 {
-	public static class HttpRequestBaseExtensions
-	{
-		/// <summary>
-		/// Returns a boolean value indicating whether this request can render debugging
-		/// information to the response such as exception details or profiling results.
-		/// </summary>
-		public static bool CanDisplayDebuggingDetails(this HttpRequestBase request)
-		{
-			return request.IsLocal;
-		}
-	}
+    public static class HttpRequestBaseExtensions
+    {
+        /// <summary>
+        /// Returns a boolean value indicating whether this request can render debugging
+        /// information to the response such as exception details or profiling results.
+        /// </summary>
+        public static bool CanDisplayDebuggingDetails(this HttpRequestBase request)
+        {
+            bool authorized = false;
+
+            if (request.IsAuthenticated)
+            {
+                IPrincipal principal = request.RequestContext.HttpContext.User;
+                // authorized = principal.IsInRole(Bruttissimo.Domain.Entity.Rights.CanAccessApplicationLogs); // TODO, figure out.
+            }
+
+            bool local = false;
+
+            try
+            {
+                local = request.IsLocal;
+            }
+            catch (ArgumentException) // for some obscure reason this can throw exceptions.
+            {
+                // suppress.
+            }
+            return authorized || local;
+        }
+    }
 }
