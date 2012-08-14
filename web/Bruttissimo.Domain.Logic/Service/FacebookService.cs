42e2aa73afc2caea9cf09f50261d8eb215d3adbb
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2012-08-14 14:54:16 -0300

    added importer repo interface, and import job flow spec.

--- web/Bruttissimo.Domain.Logic/Service/FacebookService.cs
+++ web/Bruttissimo.Domain.Logic/Service/FacebookService.cs
@@ -8,8 +8,9 @@ namespace Bruttissimo.Domain.Logic
     public class FacebookService : IFacebookService
     {
         private readonly IFacebookRepository facebookRepository;
+        private readonly IImporterRepository importerRepository;
 
-        public FacebookService(IFacebookRepository facebookRepository)
+        public FacebookService(IFacebookRepository facebookRepository, IImporterRepository importerRepository)
         {
             if (facebookRepository == null)
             {
@@ -21,8 +22,23 @@ namespace Bruttissimo.Domain.Logic
         public void Import()
         {
             Thread.Sleep(300000);
-            DateTime? since = null;
+            DateTime? since = importerRepository.GetLastImportDate(ImportSource.Facebook);
             IEnumerable<FacebookPost> feed = facebookRepository.GetPostsInGroupFeed(null, since);
+
+            // TODO: filter already-saved or posts with existing links.
+            // TODO: save posts to DB.
+            importerRepository.UpdateLastImportDate(ImportSource.Facebook);
         }
+    }
+
+    public interface IImporterRepository
+    {
+        DateTime? GetLastImportDate(ImportSource source);
+        void UpdateLastImportDate(ImportSource source);
+    }
+
+    public enum ImportSource
+    {
+        Facebook
     }
 }
