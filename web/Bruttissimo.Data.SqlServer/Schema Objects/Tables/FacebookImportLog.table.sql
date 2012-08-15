c246cfdcdf586b19cb67373a064dbb7a4bcf71d3
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2012-08-15 13:41:21 -0300

    updated fb import table def

---
+++ web/Bruttissimo.Data.SqlServer/Schema Objects/Tables/FacebookImportLog.table.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[FacebookImportLog](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+    [FacebookFeedId]  NVARCHAR (MAX) NULL,
+	[Date] [datetime2](7) NOT NULL
+);
--- web/Bruttissimo.Data.SqlServer/Schema Objects/Tables/Import.table.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].[Import](
-	[Id] [bigint] IDENTITY(1,1) NOT NULL,
-	[Date] [datetime2](7) NOT NULL,
-	[Source] [tinyint] NOT NULL
-);
