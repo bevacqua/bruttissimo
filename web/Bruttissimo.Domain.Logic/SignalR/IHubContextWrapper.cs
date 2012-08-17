dbb348b6a3caf185427b90378a158ee4a568de1f
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2012-08-17 07:43:41 -0300

    added clarification comment.

--- web/Bruttissimo.Domain.Logic/SignalR/IHubContextWrapper.cs
+++ web/Bruttissimo.Domain.Logic/SignalR/IHubContextWrapper.cs
@@ -2,7 +2,7 @@ using SignalR.Hubs;
 
 namespace Bruttissimo.Domain
 {
-    public interface IHubContextWrapper<THub> where THub : IHub
+    public interface IHubContextWrapper<THub> where THub : IHub // THub reference is used for resolving with IoC container.
     {
         IHubContext Context { get; }
     }
