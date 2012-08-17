aa5737c59d30476a0c43afab4fc97b7b8f1d1577
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2012-08-17 17:47:37 -0300

    moved json entities

---
+++ web/Bruttissimo.Domain.Social/Facebook/Entity/FacebookResponsePaging.cs
@@ -0,0 +1,8 @@
+namespace Bruttissimo.Domain.Social
+{
+    internal class FacebookResponsePaging
+    {
+        public string Previous { get; set; }
+        public string Next { get; set; }
+    }
+}
--- web/Bruttissimo.Domain.Social/Facebook/FacebookResponsePaging.cs
+++
@@ -1,8 +0,0 @@
-namespace Bruttissimo.Domain.Social
-{
-    public class FacebookResponsePaging
-    {
-        public string Previous { get; set; }
-        public string Next { get; set; }
-    }
-}
