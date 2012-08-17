fb5d43a24480922b184ab99eda32e46fce28e3a9
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2012-08-17 12:31:48 -0300

    made SQL post-deployment script idempotent.

--- web/Bruttissimo.Data.SqlServer/Scripts/Post-Deployment/Script.PostDeployment.sql
+++ web/Bruttissimo.Data.SqlServer/Scripts/Post-Deployment/Script.PostDeployment.sql
@@ -10,23 +10,56 @@ Post-Deployment Script Template
 --------------------------------------------------------------------------------------
 */
 
-INSERT INTO [UserRole] ([Name])
-SELECT 'Regular'
-UNION ALL
-SELECT 'Admin'
+IF NOT EXISTS(SELECT NULL FROM UserRole WHERE Name = 'Regular')
+    INSERT INTO UserRole VALUES('Regular')
 
-INSERT INTO [UserRight] ([Name])
-SELECT 'CanAccessSystemPanel'
-UNION ALL
-SELECT 'CanAccessApplicationLogs'
-UNION ALL
-SELECT 'CanAccessApplicationJobs'
+IF NOT EXISTS(SELECT NULL FROM UserRole WHERE Name = 'Admin')
+    INSERT INTO UserRole VALUES('Admin')
 
-INSERT INTO [UserRoleRight] ([UserRoleId], [UserRightId])
-SELECT	[Ro].[Id], [Ri].[Id] FROM [UserRight] Ri, [UserRole] Ro
- WHERE ([Ro].[Name] = 'Admin' AND
-		[Ri].[Name] = 'CanAccessSystemPanel')
-    OR ([Ro].[Name] = 'Admin' AND
-		[Ri].[Name] = 'CanAccessApplicationLogs')
-    OR ([Ro].[Name] = 'Admin' AND
-		[Ri].[Name] = 'CanAccessApplicationJobs')
+IF NOT EXISTS(SELECT NULL FROM UserRight WHERE Name = 'CanAccessSystemPanel')
+    INSERT INTO UserRight VALUES('CanAccessSystemPanel')
+
+IF NOT EXISTS(SELECT NULL FROM UserRight WHERE Name = 'CanAccessApplicationLogs')
+    INSERT INTO UserRight VALUES('CanAccessApplicationLogs')
+
+IF NOT EXISTS(SELECT NULL FROM UserRight WHERE Name = 'CanAccessApplicationJobs')
+    INSERT INTO UserRight VALUES('CanAccessApplicationJobs')
+
+IF NOT EXISTS(
+    SELECT NULL FROM UserRoleRight RR
+    WHERE RR.UserRoleId  = (SELECT Ro.Id FROM UserRole  Ro WHERE Ro.Name = 'Admin')
+      AND RR.UserRightId = (SELECT Ri.Id FROM UserRight Ri WHERE Ri.Name = 'CanAccessSystemPanel')
+)
+BEGIN
+    INSERT INTO UserRoleRight (UserRoleId, UserRightId)
+    SELECT	Ro.Id, Ri.Id
+      FROM  UserRight Ri, UserRole Ro
+     WHERE (Ro.Name = 'Admin' AND
+		    Ri.Name = 'CanAccessSystemPanel')
+END
+
+IF NOT EXISTS(
+    SELECT NULL FROM UserRoleRight RR
+    WHERE RR.UserRoleId  = (SELECT Ro.Id FROM UserRole  Ro WHERE Ro.Name = 'Admin')
+      AND RR.UserRightId = (SELECT Ri.Id FROM UserRight Ri WHERE Ri.Name = 'CanAccessApplicationLogs')
+)
+BEGIN
+    INSERT INTO UserRoleRight (UserRoleId, UserRightId)
+    SELECT	Ro.Id, Ri.Id
+      FROM  UserRight Ri, UserRole Ro
+     WHERE (Ro.Name = 'Admin' AND
+		    Ri.Name = 'CanAccessApplicationLogs')
+END
+
+IF NOT EXISTS(
+    SELECT NULL FROM UserRoleRight RR
+    WHERE RR.UserRoleId  = (SELECT Ro.Id FROM UserRole  Ro WHERE Ro.Name = 'Admin')
+      AND RR.UserRightId = (SELECT Ri.Id FROM UserRight Ri WHERE Ri.Name = 'CanAccessApplicationJobs')
+)
+BEGIN
+    INSERT INTO UserRoleRight (UserRoleId, UserRightId)
+    SELECT	Ro.Id, Ri.Id
+      FROM  UserRight Ri, UserRole Ro
+     WHERE (Ro.Name = 'Admin' AND
+		    Ri.Name = 'CanAccessApplicationJobs')
+END
