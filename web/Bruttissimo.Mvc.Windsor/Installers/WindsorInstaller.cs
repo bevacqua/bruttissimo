c0fbd8c02c1486c2680a55a134a522a8c0586a90
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2012-08-24 14:08:04 -0300

    composition root changed

--- web/Bruttissimo.Mvc.Windsor/Installers/WindsorInstaller.cs
+++ web/Bruttissimo.Mvc.Windsor/Installers/WindsorInstaller.cs
@@ -13,7 +13,7 @@ using Castle.Windsor;
 namespace Bruttissimo.Mvc.Windsor
 {
     /// <summary>
-    /// Installs all dependencies into the container. Acts as the composition root.
+    /// Installs all dependencies into the container.
     /// </summary>
     public class WindsorInstaller : IWindsorInstaller
     {
