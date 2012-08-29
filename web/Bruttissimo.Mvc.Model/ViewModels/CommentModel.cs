da9882a015c163a267e1a894adbf5a62c508b19e
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2012-08-29 18:05:12 -0300

    adding comment list view and model

---
+++ web/Bruttissimo.Mvc.Model/ViewModels/CommentModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Bruttissimo.Mvc.Model
+{
+    public class CommentModel
+    {
+        public long Id { get; set; }
+        public long? ParentId { get; set; }
+        public string Message { get; set; }
+
+        public IList<CommentModel> Comments { get; set; }
+
+        public bool HasComments
+        {
+            get { return Comments.Count > 0; }
+        }
+    }
+}
