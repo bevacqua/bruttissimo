4288825265102032a2f7810c67ffe63c7765de02
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2012-09-06 16:11:10 -0300

    database deployment configuration
    - renamed core database to Bruttissimo

--- web/Bruttissimo.Data.SqlServer/Schema Objects/Storage/Files/Bruttissimo_Debug.sqlfile.sql
+++ web/Bruttissimo.Data.SqlServer/Schema Objects/Storage/Files/Bruttissimo_Debug.sqlfile.sql
@@ -1,3 +1,3 @@
 ﻿ALTER DATABASE [$(DatabaseName)]
-    ADD FILE (NAME = [Bruttissimo_Debug], FILENAME = '$(DefaultDataPath)$(DatabaseName).mdf', FILEGROWTH = 1024 KB) TO FILEGROUP [PRIMARY];
+    ADD FILE (NAME = [$(DatabaseName)], FILENAME = '$(DefaultDataPath)$(DatabaseName).mdf', FILEGROWTH = 1024 KB) TO FILEGROUP [PRIMARY];
 
--- web/Bruttissimo.Data.SqlServer/Schema Objects/Storage/Files/Bruttissimo_Debug_log.sqlfile.sql
+++ web/Bruttissimo.Data.SqlServer/Schema Objects/Storage/Files/Bruttissimo_Debug_log.sqlfile.sql
@@ -1,3 +1,3 @@
 ﻿ALTER DATABASE [$(DatabaseName)]
-    ADD LOG FILE (NAME = [Bruttissimo_Debug_log], FILENAME = '$(DefaultLogPath)$(DatabaseName)_log.ldf', MAXSIZE = 2097152 MB, FILEGROWTH = 10 %);
+    ADD LOG FILE (NAME = [$(DatabaseName)_log], FILENAME = '$(DefaultLogPath)$(DatabaseName)_log.ldf', MAXSIZE = 2097152 MB, FILEGROWTH = 10 %);
 
