0c786cdaa214b0f0686134d6f627121df2d0c117
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2012-09-06 15:47:25 -0300

    DbUp build step self-execution
    closes #80

---
+++ web/Bruttissimo.Data.Deployment/DbUp/DbUpgrader.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using Bruttissimo.Common;
+using Bruttissimo.Extensions.MiniProfiler;
+using DbUp;
+using DbUp.Engine;
+using DbUp.Engine.Output;
+using StackExchange.Profiling;
+using StackExchange.Profiling.Data;
+using log4net;
+
+namespace Bruttissimo.Data.Deployment
+{
+    public class DbUpgrader
+    {
+        private readonly ILog logger;
+        private readonly IUpgradeLog log;
+        private readonly Stopwatch stopwatch;
+        private readonly IList<IDbConnection> connections;
+
+        public DbUpgrader()
+        {
+            logger = LogManager.GetLogger(typeof(UpgradeEngine));
+            log = new Log4NetAndConsoleUpgradeLog(logger);
+            stopwatch = new Stopwatch();
+            connections = new List<IDbConnection>();
+        }
+
+        public int Execute()
+        {
+            Assembly assembly = typeof(Program).Assembly;
+            
+            stopwatch.Restart();
+
+            UpgradeEngine upgrader = DeployChanges.To
+                .SqlDatabase(GetConnectionString)
+                .WithScriptsEmbeddedInAssembly(assembly)
+                .LogTo(log)
+                .Build();
+
+            DatabaseUpgradeResult result = upgrader.PerformUpgrade();
+            DisposeDbConnections();
+
+            int exitCode = GetExitCode(result);
+            return exitCode;
+        }
+
+        private int GetExitCode(DatabaseUpgradeResult result)
+        {
+            if (!result.Successful)
+            {
+                logger.Error("An exception occurred while upgrading the database.", result.Error);
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+                return -1;
+            }
+            else
+            {
+                int count = result.Scripts.Count();
+
+                stopwatch.Stop();
+                TimeSpan elapsed = stopwatch.Elapsed;
+                string duration = elapsed.ToString("mm':'ss");
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("{0} scripts executed successfully. Done in {1}!", count, duration);
+                Console.ResetColor();
+                return 0;
+            }
+        }
+
+        internal IDbConnection GetConnectionString()
+        {
+            string connectionString = Config.GetConnectionString("SqlConnection");
+            DbConnection connection = new SqlConnection(connectionString);
+            RichErrorDbConnection profiled = new RichErrorDbConnection(connection, MiniProfiler.Current); // wraps MiniProfiler's ProfiledDbConnection.
+            connections.Add(profiled);
+            return profiled;
+        }
+        
+        internal void DisposeDbConnections()
+        {
+            foreach(IDbConnection connection in connections)
+            {
+                DisposeDbConnection(connection);
+            }
+        }
+
+        internal void DisposeDbConnection(IDbConnection connection)
+        {
+            ProfiledDbConnection profiled = connection as ProfiledDbConnection;
+            if (profiled != null) // for some reason, MiniProfiler profiled Db Connections are disposed at some point.
+            {
+                if (profiled.WrappedConnection != null)
+                {
+                    profiled.WrappedConnection.Close();
+                }
+            }
+            else
+            {
+                connection.Close();
+            }
+        }
+    }
+}
