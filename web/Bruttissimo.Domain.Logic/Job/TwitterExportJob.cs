01e14aca83acd91e229b0920e86cc00203eb970a
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2012-09-08 23:05:05 -0300

    twitter export job stub
    #14

---
+++ web/Bruttissimo.Domain.Logic/Job/TwitterExportJob.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Bruttissimo.Common;
+using Quartz;
+
+namespace Bruttissimo.Domain.Logic
+{
+    // [AutoRun]
+    public class TwitterExportJob : BaseJob
+    {
+        private readonly IFacebookService facebookService;
+        
+        public TwitterExportJob(IFacebookService facebookService)
+        {
+            if (facebookService == null)
+            {
+                throw new ArgumentNullException("facebookService");
+            }
+            this.facebookService = facebookService;
+        }
+
+        public override void DoWork(IJobExecutionContext context)
+        {
+            facebookService.Export();
+        }
+    }
+}
