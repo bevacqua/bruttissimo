c437bae90f2dd4480869132d4dea85ce9bbceb4f
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2012-09-17 18:49:50 -0400

    added tests to ensure post mappings work as intended.

--- web/Bruttissimo.Tests.Integration/AutoMapperTests.cs
+++ web/Bruttissimo.Tests.Integration/AutoMapperTests.cs
@@ -1,8 +1,9 @@
 ﻿using Bruttissimo.Common;
+using Bruttissimo.Domain.Entity;
+using Bruttissimo.Mvc.Model;
 using Bruttissimo.Tests.Mocking;
 using Castle.Windsor;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Mapper = AutoMapper.Mapper;
 
 namespace Bruttissimo.Tests.Integration
 {
@@ -29,6 +30,46 @@ namespace Bruttissimo.Tests.Integration
         public void AutoMapperShouldBeProperlyConfigured()
         {
             mapper.AssertConfigurationIsValid();
+        }
+
+        [TestMethod]
+        public void PostShouldMapToConcreteLinkPostModel()
+        {
+            Post post = new Post { Link = new Link { Type = LinkType.Html } };
+
+            PostModel model = mapper.Map<Post, PostModel>(post);
+
+            Assert.IsInstanceOfType(model, typeof(LinkPostModel));
+        }
+
+        [TestMethod]
+        public void PostShouldMapPropertiesToConcreteLinkPostModel()
+        {
+            Post post = new Post { Link = new Link { Type = LinkType.Html, Description = "desc" } };
+
+            PostModel model = mapper.Map<Post, PostModel>(post);
+
+            Assert.AreEqual("desc", ((LinkPostModel)model).LinkDescription);
+        }
+
+        [TestMethod]
+        public void PostShouldMapToConcreteImagePostModel()
+        {
+            Post post = new Post { Link = new Link { Type = LinkType.Image } };
+
+            PostModel model = mapper.Map<Post, PostModel>(post);
+
+            Assert.IsInstanceOfType(model, typeof(ImagePostModel));
+        }
+
+        [TestMethod]
+        public void PostShouldMapPropertiesToConcreteImagePostModel()
+        {
+            Post post = new Post { Link = new Link { Type = LinkType.Image, Picture = "pic" } };
+
+            PostModel model = mapper.Map<Post, PostModel>(post);
+
+            Assert.AreEqual("pic", ((ImagePostModel)model).LinkPicture);
         }
     }
 }
